f8e9aca20967005048881253c690e8db865e31d9
Jim Shaw <jimxshaw@gmail.com>
2021-03-31 19:49:29 -0500

    Refactor double quotes into single quotes.

--- i_have_money/lib/main.dart
+++ i_have_money/lib/main.dart
@@ -6,8 +6,13 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("I Have Money!"),
-          backgroundColor: HexColor("#0E7AFE"),
+          title: Text('I Have Money!'),
+          backgroundColor: HexColor('#0E7AFE'),
+        ),
+        backgroundColor: HexColor('#7D7D7D'),
+        body: Image(
+          image: NetworkImage(
+              'https://thejewelerblog.files.wordpress.com/2017/07/carmenlucia2.jpg'),
         ),
       ),
     ),
--- i_have_money/lib/util/HexColor.dart
+++ i_have_money/lib/util/HexColor.dart
@@ -4,10 +4,10 @@ class HexColor extends Color {
   HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
 
   static int _getColorFromHex(String hexColor) {
-    hexColor = hexColor.toUpperCase().replaceAll("#", "");
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
 
     if (hexColor.length == 6) {
-      hexColor = "FF" + hexColor;
+      hexColor = 'FF' + hexColor;
     }
 
     return int.parse(hexColor, radix: 16);
