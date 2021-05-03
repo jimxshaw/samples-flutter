afa5e00bed6dedad47136ee0c3e4109f545e50b3
Jim Shaw <jimxshaw@gmail.com>
2021-05-03 11:19:55 -0500

    Add stateful widget.

--- magic-8/lib/main.dart
+++ magic-8/lib/main.dart
@@ -4,8 +4,32 @@ import 'package:magic_8_ball/util/hexcolor.dart';
 void main() => runApp(
       MaterialApp(
         home: Scaffold(
-          backgroundColor: HexColor('#E1165E'),
-
+          backgroundColor: HexColor('#673AB7'),
+          appBar: AppBar(
+            title: Text('Ask Me Anything'),
+            centerTitle: true,
+            backgroundColor: HexColor('#512DA8'),
+          ),
+          body: Ball(),
         ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          print('I got pressed!');
+        },
+        child: Image.asset('images/ball1.png'),
+      ),
+    );
+  }
+}
