779184b91a402dd5bc4d7186cf0604c24414906e
Jim Shaw <jimxshaw@gmail.com>
2021-05-03 11:06:31 -0500

    Add Magic 8 boilerplate app.

---
+++ magic-8/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:magic_8_ball/util/hexcolor.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: HexColor('#E1165E'),
+
+        ),
+      ),
+    );
