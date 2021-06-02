43bbd103cd981e5fdb71383db14774f509bb09e5
Jim Shaw <jimxshaw@gmail.com>
2021-06-01 22:05:40 -0500

    Add QuizEngine class.

---
+++ quiz_app/lib/quiz_engine.dart
@@ -0,0 +1,9 @@
+import 'question.dart';
+
+class QuizEngine {
+  List<Question> questionList = [
+    new Question('Hispania was a Roman Empire province.',true),
+    new Question('A spider is an insect.',false),
+    new Question('The closest star to Earth is the Sun.',true)
+  ];
+}
