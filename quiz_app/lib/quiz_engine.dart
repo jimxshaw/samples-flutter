f6328117ea3f573f0f9fc05093a2072a8cafe857
Jim Shaw <jimxshaw@gmail.com>
2021-06-01 22:21:10 -0500

    Add nextQuestion method.

--- quiz_app/lib/quiz_engine.dart
+++ quiz_app/lib/quiz_engine.dart
@@ -1,17 +1,25 @@
 import 'question.dart';
 
 class QuizEngine {
+  int _questionNumber = 0;
+
   List<Question> _questionList = [
     new Question('Hispania was a Roman Empire province.',true),
     new Question('A spider is an insect.',false),
     new Question('The closest star to Earth is the Sun.',true)
   ];
 
-  String getQuestionText(int questionNumber) {
-    return _questionList[questionNumber].questionText;
+  void nextQuestion() {
+    if (_questionNumber < _questionList.length) {
+      _questionNumber++;
+    }
   }
 
-  bool getQuestionAnswer(int questionAnswer) {
-    return _questionList[questionAnswer].questionAnswer;
+  String getQuestionText() {
+    return _questionList[_questionNumber].questionText;
+  }
+
+  bool getQuestionAnswer() {
+    return _questionList[_questionNumber].questionAnswer;
   }
 }
