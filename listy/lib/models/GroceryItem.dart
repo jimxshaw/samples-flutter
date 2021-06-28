552cb590024a1884363fb5cb101521ad56fcdcd4
Jim Shaw <jimxshaw@gmail.com>
2021-06-27 19:54:38 -0500

    Add Category enum.

--- listy/lib/models/GroceryItem.dart
+++ listy/lib/models/GroceryItem.dart
@@ -1,12 +1,24 @@
+enum Category {
+  Produce,
+  Bakery,
+  Dairy,
+  Frozen,
+  Aisle,
+  Meat,
+  Household,
+  Misc,
+}
+
 class GroceryItem {
   int? id;
   String? name;
-  // category
+  Category? category;
   bool purchased = false;
 
   GroceryItem.fromJason(Map<String, dynamic> json) {
     this.id = json['id'];
     this.name = json['name'];
+    this.category = json['category'];
     this.purchased = json['purchased'];
   }
 }
