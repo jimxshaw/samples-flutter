9007f314e3acc29123dace9e1145620852593234
Jim Shaw <jimxshaw@gmail.com>
2021-06-27 21:14:42 -0500

    Use grocery items mock in list screen.

--- listy/lib/screens/list_screen.dart
+++ listy/lib/screens/list_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:listy/mocks/grocery_items_mock.dart';
 
 class ListScreen extends StatelessWidget {
   const ListScreen({key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final items = groceryItems;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("My list"),
@@ -13,6 +16,15 @@ class ListScreen extends StatelessWidget {
         onPressed: () {},
         child: Icon(Icons.add),
       ),
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index];
+
+            return Card(
+              child: Text(item['name']),
+            );
+          }),
     );
   }
 }
