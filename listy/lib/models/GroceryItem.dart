43c6b7b51790eae9c73d2b1aef1fe0f328d7c0be
Jim Shaw <jimxshaw@gmail.com>
2021-06-27 20:03:09 -0500

    Add map const, add get methods.

--- listy/lib/models/GroceryItem.dart
+++ listy/lib/models/GroceryItem.dart
@@ -9,6 +9,17 @@ enum Category {
   Misc,
 }
 
+const GroceryItemCategoryMap = {
+  Category.Produce: "produce",
+  Category.Bakery: "bakery",
+  Category.Dairy: "dairy",
+  Category.Frozen: "frozen",
+  Category.Aisle: "aisle",
+  Category.Meat: "meat",
+  Category.Household: "household",
+  Category.Misc: "misc",
+};
+
 class GroceryItem {
   int? id;
   String? name;
@@ -20,5 +31,15 @@ class GroceryItem {
     this.name = json['name'];
     this.category = json['category'];
     this.purchased = json['purchased'];
+  }
+
+  static Category getCategoryFromString(String category) {
+    return GroceryItemCategoryMap.entries
+                                 .firstWhere((element) => element.value == category)
+                                 .key;
+  }
+
+  static String? getStringFromCategory(Category category) {
+    return GroceryItemCategoryMap[category];
   }
 }
