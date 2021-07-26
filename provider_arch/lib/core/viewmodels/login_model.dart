817fee842239472aa7d56b83133c3d9a8464e06b
Jim Shaw <jimxshaw@gmail.com>
2021-07-26 18:00:08 -0500

    Add implementation to LoginModel.

--- provider_arch/lib/core/viewmodels/login_model.dart
+++ provider_arch/lib/core/viewmodels/login_model.dart
@@ -10,4 +10,25 @@ enum ViewState { Idle, Busy }
 class LoginModel extends ChangeNotifier {
   final AuthenticationService _authenticationService =
       locator<AuthenticationService>();
+
+  ViewState _state = ViewState.Idle;
+
+  ViewState get state => _state;
+
+  void setState(ViewState viewState) {
+    _state = viewState;
+
+    notifyListeners();
+  }
+
+  Future<bool> login(String userIdText) async {
+    setState(ViewState.Busy);
+
+    var userId = int.tryParse(userIdText);
+    var success = await _authenticationService.login(userId);
+
+    setState(ViewState.Idle);
+
+    return success;
+  }
 }
