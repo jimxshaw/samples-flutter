b87980d4011d235ce318daa39c41a5cdcf9adbe5
Jim Shaw <jimxshaw@gmail.com>
2021-07-26 17:12:59 -0500

    Add boilerplate ChangeNotifierProvider to LoginView.

--- provider_arch/lib/ui/views/login_view.dart
+++ provider_arch/lib/ui/views/login_view.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_architecture/core/viewmodels/login_model.dart';
+import 'package:provider_architecture/locator.dart';
 
 class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ChangeNotifierProvider<LoginModel>(
+      create: (context) => locator<LoginModel>(),
+      child: Consumer<LoginModel>(
+        builder: (context, model, child) => Scaffold(),
+      ),
+    );
   }
 }
