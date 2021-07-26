77e4ea4a010333c2450944ba6ea4b850b66f25f0
Jim Shaw <jimxshaw@gmail.com>
2021-07-26 17:54:56 -0500

    Add AuthenticationService and use it in LoginModel.

--- provider_arch/lib/core/viewmodels/login_model.dart
+++ provider_arch/lib/core/viewmodels/login_model.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/widgets.dart';
 
 import 'package:flutter/foundation.dart';
+import 'package:provider_architecture/core/services/authentication_service.dart';
+import 'package:provider_architecture/locator.dart';
 
 // Provides the state of the view.
 enum ViewState { Idle, Busy }
 
-class LoginModel extends ChangeNotifier {}
+class LoginModel extends ChangeNotifier {
+  final AuthenticationService _authenticationService =
+      locator<AuthenticationService>();
+}
