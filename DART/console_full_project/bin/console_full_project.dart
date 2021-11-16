766c4a7389d53b3e3be5dbe02d4200ec23d9bd59
Jim Shaw <jimxshaw@gmail.com>
2021-11-15 21:31:38 -0600

    Update console_full_project sample.

--- DART/console_full_project/bin/console_full_project.dart
+++ DART/console_full_project/bin/console_full_project.dart
@@ -1,5 +1,11 @@
-import 'package:console_full_project/console_full_project.dart' as console_full_project;
+import 'package:console_full_project/console_full_project.dart'
+    as console_full_project;
 
 void main(List<String> arguments) {
-  print('Hello world: ${console_full_project.calculate()}!');
+  try {
+    print(
+        'Args sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}');
+  } catch (e) {
+    print('Error: at least one element is not an integer.');
+  }
 }
