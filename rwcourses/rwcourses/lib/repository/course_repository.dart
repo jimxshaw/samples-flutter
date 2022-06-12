1d4dac3d3d4c8a9e7499605528d32080c72d4f4a
Jim Shaw <jimxshaw@gmail.com>
2022-06-12 13:00:27 -0500

    Parse json in CourseRepository and have it display in CoursesPage.

--- rwcourses/rwcourses/lib/repository/course_repository.dart
+++ rwcourses/rwcourses/lib/repository/course_repository.dart
@@ -20,8 +20,11 @@ class CourseRepository implements Repository {
     final uri = Uri.parse(url);
     final response = await http.get(uri);
     final apiResponse = json.decode(response.body) as Map<String, dynamic>;
+    final data = apiResponse['data'] as List;
 
-    print(apiResponse);
+    for (final json in data) {
+      courses.add(Course.fromJson(json as Map<String, dynamic>));
+    }
 
     return courses;
   }
