bdbfa486405edf255f7827c99192f4aa4cafecbd
Jim Shaw <jimxshaw@gmail.com>
2022-06-12 10:46:50 -0500

    Update RWCourses widget to have app bar.

--- rwcourses/rwcourses/lib/rwcourses_app.dart
+++ rwcourses/rwcourses/lib/rwcourses_app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'strings.dart';
 
 class RWCoursesApp extends StatelessWidget {
   const RWCoursesApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Strings.appTitle),
+      ),
+    );
   }
 }
