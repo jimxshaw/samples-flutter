9ab845aa97256316db5d3fbdeec86f24994e82bc
Jim Shaw <jimxshaw@gmail.com>
2022-06-12 10:45:00 -0500

    Create RWCoursesApp boilerplate widget.

---
+++ rwcourses/rwcourses/lib/rwcourses_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RWCoursesApp extends StatelessWidget {
+  const RWCoursesApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
