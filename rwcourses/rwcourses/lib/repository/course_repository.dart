efa457e401b7badd281c608cb187f0180e2462e7
Jim Shaw <jimxshaw@gmail.com>
2022-06-12 11:58:21 -0500

    Create course repository boilerplate.

---
+++ rwcourses/rwcourses/lib/repository/course_repository.dart
@@ -0,0 +1,22 @@
+import 'dart:html';
+
+import '../model/course.dart';
+import '../repository/repository.dart';
+import '../constants.dart';
+
+class CourseRepository implements Repository {
+  String dataURL =
+      'https://api.raywenderlich.com/api/contents?filter[content_types][]=collection';
+
+  @override
+  Future<List<Course>> getCourses(int domainFilter) async {
+    final courses = <Course>[];
+    var url = dataURL;
+
+    if (domainFilter != Constants.allFilter) {
+      url += ';&filter[domain_ids][]=$domainFilter';
+    }
+
+    return courses;
+  }
+}
