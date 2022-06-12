b6d1f3e6319f972c1e1964f9a71ecb0b0a6952ef
Jim Shaw <jimxshaw@gmail.com>
2022-06-12 10:55:09 -0500

    Update main.dart to use RWCourseApp widget as home and remove tests folder.

--- rwcourses/rwcourses/lib/main.dart
+++ rwcourses/rwcourses/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:rwcourses/rwcourses_app.dart';
+import 'strings.dart';
 
 void main() => runApp(
-      const MaterialApp(),
+      MaterialApp(
+        title: Strings.appTitle,
+        theme: ThemeData(primaryColor: Colors.green.shade800),
+        home: const RWCoursesApp(),
+      ),
     );
