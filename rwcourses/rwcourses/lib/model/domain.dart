5d31a35a11331418e89e1daae781d2eb71823579
Jim Shaw <jimxshaw@gmail.com>
2022-06-13 09:52:03 -0500

    Add DomainExtension on Domain enum.

--- rwcourses/rwcourses/lib/model/domain.dart
+++ rwcourses/rwcourses/lib/model/domain.dart
@@ -1,3 +1,5 @@
+import '../strings.dart';
+
 enum Domain {
   all,
   ios,
@@ -8,4 +10,26 @@ enum Domain {
   macos,
   archived,
   unknown,
+}
+
+extension DomainExtension on Domain {
+  String get name {
+    switch (this) {
+      case Domain.ios:
+        return Strings.ios;
+      case Domain.android:
+        return Strings.android;
+      case Domain.flutter:
+        return Strings.flutter;
+      case Domain.sss:
+        return Strings.sss;
+      case Domain.unity:
+        return Strings.unity;
+      case Domain.macos:
+        return Strings.macos;
+      case Domain.archived:
+      default:
+        return Strings.unknown;
+    }
+  }
 }
